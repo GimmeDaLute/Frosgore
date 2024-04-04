da50958298261d3771f27a3fe1730e9eaa112c33
GimmeDaLute <peter.m.edwards@outlook.com>
2024-04-04 20:22:59 +0100

    Added map image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+import logo from './layer.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Frosgore RP
         </p>
+        <img src={logo} alt="Frosgore logo" />
       </header>
     </div>
   );
